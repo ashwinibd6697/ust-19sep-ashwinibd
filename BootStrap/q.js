0ae3c326d849a2d552a197611f5deef6948774d4
ashwinibd6697 <ashwinibd6697@gmail.com>
2019-11-22 11:35:28 +0530

    Bootstrap

---
+++ BootStrap/q.js
@@ -0,0 +1,3 @@
+function showDetails(){
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam ea similique temporibus pariatur error autem assumenda incidunt, recusandae eius esse reiciendis aut quod natus, numquam mollitia doloremque totam sint et vel beatae dicta sequi? Quae deserunt voluptates nisi dolorum quas!</p>
+}
