0ecc649b7f103943648fab1b10b4772416bcc715
ashwinibd6697 <ashwinibd6697@gmail.com>
2019-12-17 11:02:37 +0530

    has a relations in java

---
+++ hasarelations/src/com/ustglobal/hasarelations/Car.java
@@ -0,0 +1,9 @@
+package com.ustglobal.hasarelations;
+
+public class Car {
+	String brand = "BMW";
+	Music m = new Music();
+	void drive() {
+		System.out.println("car driving is nice");
+	}
+}
---
+++ hasarelations/src/com/ustglobal/hasarelations/Person.java
@@ -0,0 +1,13 @@
+package com.ustglobal.hasarelations;
+
+public class Person {
+   String name = "Ashwini";
+   Marker m = new Marker();
+    void sleep() {
+    	System.out.println("sleep() method");
+    }
+void eat() {
+	System.out.println("eat() method");
+}
+
+}
